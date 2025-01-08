91075b8d4f551ce7994853609f1a4e31fdd6d4dd
Edwin Ramos <edwin.ramos@policia.bo>
2025-01-08 10:31:53 -0400

    adding dist

---
+++ assets/Dashboard-Bn3gdHS6.js
@@ -0,0 +1,2 @@
+import{d as t,u as a,o as n,c as r,a as c,t as l,b as u}from"./index-f4UEXm-2.js";const m=t({__name:"Dashboard",setup(p){var o;const e=a(),s=e.getModules();return console.log((o=e.tokens)==null?void 0:o.access_token),(_,d)=>(n(),r("div",null,[c("pre",null,"            "+l(JSON.stringify(u(s)))+`
+        `,1)]))}});export{m as default};
